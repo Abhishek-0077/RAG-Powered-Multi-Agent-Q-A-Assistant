485ac020237821c56f5ae317569fbbad21f1afb4
Abhishek Kumar <139259158+Abhishek-0077@users.noreply.github.com>
2025-05-10 11:47:40 +0000

    Changed for Route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect, url_for
+from model_For_Rag import classify_query_with_chroma
 
 app = Flask(__name__)
 
@@ -9,7 +10,8 @@ def index():
     global chat_history
     if request.method == "POST":
         user_input = request.form["query"]
-        bot_reply = "OK"  # Replace with real response later
+        route =  classify_query_with_chroma(user_input, top_k=3)  # Replace with real response later
+        bot_reply = f"Route for this Query: {str(route).upper()}"
         chat_history.append(("user", user_input))
         chat_history.append(("bot", bot_reply))
         return redirect(url_for("index"))
